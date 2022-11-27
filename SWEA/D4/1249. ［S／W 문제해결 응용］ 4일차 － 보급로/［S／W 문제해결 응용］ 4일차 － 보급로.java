c4550839922ed93056ede253f7e24a6e238f6d48
haemin <44861558+leepeace@users.noreply.github.com>
2022-11-27 10:35:01 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 168 ms, Memory: 37,588 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -8,97 +8,98 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Solution{
-	private static int result;
-	private static int N;
-	private static boolean[][] visited;
-	private static int[][] maps;
 	
-	static class Point implements Comparable<Point>{
+	static int[][] maps;
+	static int result;
+	static int N;
+	static int[][] dir = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			
+			maps = new int[N][N];
+			result = Integer.MAX_VALUE;
+			
+			for (int i = 0; i < N; i++) {
+				String tmp = br.readLine();
+				for (int j = 0; j < N; j++) {
+					maps[i][j] = tmp.charAt(j)-'0';
+				}
+			}
+		
+			solve(0, 0);
+			
+			sb.append(String.format("#%d %d\n", tc, result));
+		}
+		System.out.println(sb);
+	}
+	
+	static class Node implements Comparable<Node>{
 		int x,y;
-		int value;		
-
-		public Point(int x, int y, int value) {
+		int value;
+		
+		public Node(int x, int y, int value) {
 			super();
 			this.x = x;
 			this.y = y;
 			this.value = value;
 		}
-
+		
 		@Override
-		public int compareTo(Point o) {
+		public int compareTo(Node o) {
 			return this.value - o.value;
 		}
+	}
+	
+	static void solve(int startX, int startY) {
+		PriorityQueue<Node> queue = new PriorityQueue<Node>();
+		
+		queue.add(new Node(startX, startY, 0));
+		
+		int endX = N-1, endY = N-1;
+		boolean[][] visited = new boolean[N][N];
+		int[][] depths = new int[N][N];//경로에 대한 합 저장
+		depths[startX][startY] = maps[startX][startY];
 
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		StringBuilder sb = new StringBuilder();
-		for (int test_case = 1; test_case <= T; test_case++) {
-			N = Integer.parseInt(br.readLine());
-			
-			maps = new int[N][N];	
-			visited = new boolean[N][N];
-			
-			for (int i = 0; i < N; i++) {
-				String tmp = br.readLine();
-				for (int j = 0; j < N; j++) {
-					maps[i][j] = tmp.charAt(j) - '0';
-				}
-			}
-			
-			result = Integer.MAX_VALUE;
-			
-			solve();
-			
-			sb.append(String.format("#%d %d\n", test_case, result));
-		}
-		System.out.println(sb.toString());
-	}
-	
-	private static int[][] dir = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};//좌우상하
-	
-	private static void solve() {
-		PriorityQueue<Point> queue = new PriorityQueue<Point>();
-		queue.add(new Point(0, 0, maps[0][0]));
-		
-		int[][] sum = new int[N][N];//경로 합
-		sum[0][0] = maps[0][0];
-		
 		while(!queue.isEmpty()) {
-			Point now = queue.poll();
+			Node now = queue.poll();
 			int nowX = now.x;
 			int nowY = now.y;
 			int nowValue = now.value;
 			
 			visited[nowX][nowY] = true;
 			
-			if(nowX == N-1 && nowY == N-1) {
-				result = Math.min(result, nowValue);
+			if(nowX == endX && nowY == endY) {
+				result = Math.min(result, depths[nowX][nowY]);
 				continue;
 			}
 			
-			for (int i = 0; i < dir.length; i++) {
-				int nextX = nowX + dir[i][0];
-				int nextY = nowY + dir[i][1];
+			for (int d = 0; d < dir.length; d++) {
+				int nextX = nowX + dir[d][0];
+				int nextY = nowY + dir[d][1];
 				
-				if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) continue;
+				if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= N) continue;
+				
+				//이미 방문한 경우 이미 저장되었던 값과 탐색할 경로의 새로운 값을 비교하여 더 작은 값을 넣음
 				if(visited[nextX][nextY]) {
-					//이미 방문한 경우 현재 진행할 경로와 이미 방문했던 경로를 비교해 현재 진행할 경로가 더 작은 경우 큐에 삽입
-					if(nowValue + maps[nextX][nextY] < sum[nextX][nextY]) {
-						sum[nextX][nextY] = nowValue + maps[nextX][nextY];
-						queue.add(new Point(nextX, nextY, sum[nextX][nextY]));						
-					}			
-				}else {					
-					sum[nextX][nextY] = nowValue + maps[nextX][nextY];
-					queue.add(new Point(nextX, nextY, sum[nextX][nextY]));
+					if(depths[nextX][nextY] > nowValue + maps[nextX][nextY]) {
+						depths[nextX][nextY] = nowValue + maps[nextX][nextY];
+						queue.add(new Node(nextX, nextY, depths[nextX][nextY]));		
+					}
+				}else {
+					depths[nextX][nextY] = nowValue + maps[nextX][nextY];
+					queue.add(new Node(nextX, nextY, depths[nextX][nextY]));					
 				}
+				
 			}
+			
 		}
 		
 	}
 	
 }
-
