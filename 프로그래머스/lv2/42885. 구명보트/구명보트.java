97fe3afd79666ada5a0eaa7d62925536525f0e00
haemin <44861558+leepeace@users.noreply.github.com>
2023-03-25 10:01:21 +0900

    [level 2] Title: 구명보트, Time: 9.23 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        
+        int idx = 0;
+        
+        for(int i = people.length-1; idx <= i; i--){
+            if(people[i] + people[idx] <= limit){
+                answer++;
+                idx++;
+            }else{
+                answer++;                
+            }
+        }
+        
+        return answer;
+    }
+}
