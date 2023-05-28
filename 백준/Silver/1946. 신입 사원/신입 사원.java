ff1c7cd25c72f7d39c03d05c240babc578ac225f
haemin <44861558+leepeace@users.noreply.github.com>
2023-05-28 21:26:36 +0900

    [Silver I] Title: 신입 사원, Time: 3028 ms, Memory: 324192 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -3,11 +3,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.StringTokenizer;
-
 
 /*
 * boj 1946 신입 사원
