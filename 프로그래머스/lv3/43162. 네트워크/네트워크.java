4c60a2a64578243490d13ec9cd4ffa246d56a6bd
haemin <44861558+leepeace@users.noreply.github.com>
2023-06-05 14:05:42 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -16,12 +16,11 @@ class Solution {
     }
     
     static void solve(int now, boolean[] visited, int[][] computers){
+        visited[now] = true;
         
         for(int i = 0; i < computers[now].length; i++){
-            if(now == i) continue;//자기 자신 제외
             //인접한 노드 방문
             if(computers[now][i] == 1 && !visited[i]){
-                visited[i] = true;
                 solve(i, visited, computers);
             }
         }
