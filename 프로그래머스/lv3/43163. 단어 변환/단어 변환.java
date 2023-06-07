74e7cc6d08279cb6eafb32df8eee9b7551a526ec
haemin <44861558+leepeace@users.noreply.github.com>
2023-06-07 09:28:02 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    static int answer;
+    
+    public int solution(String begin, String target, String[] words) {
+        answer = Integer.MAX_VALUE;
+        
+        search(0, begin, target, words, new boolean[words.length]);
+        
+        answer = answer == Integer.MAX_VALUE ? 0 : answer;
+        
+        return answer;
+    }
+    
+    static void search(int cnt, String prev, String target, 
+                       String[] words, boolean[] visited){
+        //target과 같을 때
+        if(prev.equals(target)){
+            answer = Math.min(answer, cnt);
+            return;
+        }
+        
+        for(int i = 0; i < words.length; i++){
+            if(visited[i]) continue;
+            //prev와 한 개의 단어만 다른 경우
+            if(checkWord(prev, words[i])){
+                visited[i] = true;
+                search(cnt+1, words[i], target, words, visited);
+                visited[i] = false;
+            }
+            
+        }
+        
+    }
+    
+    static boolean checkWord(String before, String after){
+        int cnt = 0;
+        
+        for(int i = 0; i < before.length(); i++){
+            if(before.charAt(i) != after.charAt(i)){
+                cnt++;
+            }
+        }
+        
+        if(cnt == 1) return true;
+        
+        return false;
+    }
+    
+}
